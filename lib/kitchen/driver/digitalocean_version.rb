49717f9df7c135c993d5b36911fff8664a8b6682
Greg Fitzgerald <greg@gregf.org>
2017-09-09 11:16:48 -0400

    Preapre for release

--- lib/kitchen/driver/digitalocean_version.rb
+++ lib/kitchen/driver/digitalocean_version.rb
@@ -20,7 +20,7 @@
 module Kitchen
   module Driver
     # Version string for Digital Ocean Kitchen driver
-    DIGITALOCEAN_VERSION = '0.9.6'.freeze
+    DIGITALOCEAN_VERSION = '0.9.7'.freeze
   end
 end
 
